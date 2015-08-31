73acee3e89094a504badadf883873255ce42bd88
Emil Sauer Lynge <emillynge24@gmail.com>
2015-08-31 19:21:28 +0200

    make setup.py and refactor to make build work

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='remoteexecution',
+    version='1.0',
+    packages=['remoteexecution'],
+    url='https://github.com/emillynge/python-remoteexecution',
+    license='GNU General Public License v3.0',
+    author='Emil Sauer Lynge',
+    author_email='',
+    description='Execute remotely using a manager to start submits'
+)
