e7f844de365d0110a19c76e613a66cb47c29be15
Emil Sauer Lynge <emillynge24@gmail.com>
2015-09-16 16:00:00 +0200

    add boltons dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='',
     zip_safe = False,
     description='Execute remotely using a manager to start submits',
-    install_requires=['jsoncodecs', 'Pyro4'],
+    install_requires=['jsoncodecs', 'Pyro4', 'boltons'],
     dependency_links=['https://github.com/emillynge/python-jsoncodecs/tarball/master#egg=jsoncodecs-0.1'],
     entry_points={
     'console_scripts': [
